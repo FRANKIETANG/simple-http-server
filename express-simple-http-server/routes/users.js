034f5e9b71dc1052e069cb6e3acd99e4b8dc7160
FRANKIETANG <350558468@qq.com>
2017-12-01 20:19:45 +0800

    express-simple-http-server

--- express-simple-http-server/routes/users.js
+++ express-simple-http-server/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const users = []
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+  .get((req, res, next) => {
+    res.json(users)
+  })
+  .post((req, res)=>{
+    const user = req.body
+    users.push(user)
+    res.json(user)
+  })
 
-module.exports = router;
+module.exports = router
